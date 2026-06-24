8d003ed765feb0433722475426ebaf6407c020a1
Sagnik Chandra <98699580+Sagnik120@users.noreply.github.com>
2026-06-24 13:10:24 +0530

    Time: 0 ms (100%), Space: 19.1 MB (88.96%) - LeetHub

---
+++ 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.py
@@ -0,0 +1,7 @@
+class Solution:
+    def subsetXORSum(self, nums: List[int]) -> int:
+        total_or=0
+
+        for num in nums:
+            total_or|=num
+        return total_or*(1<<(len(nums)-1))
