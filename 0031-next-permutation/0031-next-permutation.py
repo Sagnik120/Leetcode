13bc450573586da406a4da913c63f77bbb9fd3ba
Sagnik Chandra <98699580+Sagnik120@users.noreply.github.com>
2026-06-27 14:28:03 +0530

    Time: 0 ms (100%), Space: 19.5 MB (25.79%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.py
@@ -0,0 +1,26 @@
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        # find pivot
+        i=len(nums)-2
+        while i>=0 and nums[i]>=nums[i+1]:
+            i-=1
+        
+        #  find the smallest element and swap with pivot
+        if i>=0:
+            j=len(nums)-1
+            while nums[j]<=nums[i]:
+                j-=1
+            nums[i],nums[j]=nums[j],nums[i]
+        
+        # reverse the suffix
+        left=i+1
+        right=len(nums)-1
+        while(right>=left):
+            nums[left],nums[right]=nums[right],nums[left]
+            left+=1
+            right-=1
+
+        
