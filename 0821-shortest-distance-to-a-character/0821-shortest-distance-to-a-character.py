602f3ca78ffdb83fcea6530659a7369f359c3197
Sagnik Chandra <98699580+Sagnik120@users.noreply.github.com>
2026-07-05 19:03:37 +0530

    Time: 0 ms (100%), Space: 19.5 MB (8.2%) - LeetHub

---
+++ 0821-shortest-distance-to-a-character/0821-shortest-distance-to-a-character.py
@@ -0,0 +1,20 @@
+class Solution:
+    def shortestToChar(self, s: str, c: str) -> List[int]:
+        n = len(s)
+        ans = [0] * n
+
+        # Left to right
+        prev = -float('inf')
+        for i in range(n):
+            if s[i] == c:
+                prev = i
+            ans[i] = i - prev
+
+        # Right to left
+        prev = float('inf')
+        for i in range(n - 1, -1, -1):
+            if s[i] == c:
+                prev = i
+            ans[i] = min(ans[i], prev - i)
+
+        return ans
