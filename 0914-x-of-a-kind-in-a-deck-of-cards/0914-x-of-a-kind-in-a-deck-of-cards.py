eb3f87aea5a03fb02f784e7975ed91dd8b09e542
Sagnik Chandra <98699580+Sagnik120@users.noreply.github.com>
2026-07-11 19:08:33 +0530

    Time: 5 ms (36.75%), Space: 19.8 MB (13.85%) - LeetHub

---
+++ 0914-x-of-a-kind-in-a-deck-of-cards/0914-x-of-a-kind-in-a-deck-of-cards.py
@@ -0,0 +1,11 @@
+class Solution:
+    def hasGroupsSizeX(self, deck: List[int]) -> bool:
+        frq={}
+        for num in deck:
+            frq[num]=frq.get(num,0)+1
+        
+        g = 0
+        for count in frq.values():
+            g = gcd(g, count)
+
+        return g >= 2
