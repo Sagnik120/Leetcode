23eb4f16f2953dd268a3a881eeb9bef567ae6f1c
Sagnik Chandra <98699580+Sagnik120@users.noreply.github.com>
2026-07-13 16:14:46 +0530

    Time: 8 ms (60.56%), Space: 19.5 MB (78.2%) - LeetHub

---
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -0,0 +1,33 @@
+class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+
+        if n == 0:
+            return True
+
+        # Check first position
+        if len(flowerbed) == 1:
+            if flowerbed[0] == 0:
+                flowerbed[0] = 1
+                n -= 1
+            return n <= 0
+
+        if flowerbed[0] == 0 and flowerbed[1] == 0:
+            flowerbed[0] = 1
+            n -= 1
+            if n == 0:
+                return True
+
+        # Check middle positions
+        for i in range(1, len(flowerbed) - 1):
+            if flowerbed[i-1] == 0 and flowerbed[i] == 0 and flowerbed[i+1] == 0:
+                flowerbed[i] = 1
+                n -= 1
+                if n == 0:
+                    return True
+
+        # Check last position
+        if flowerbed[-1] == 0 and flowerbed[-2] == 0:
+            flowerbed[-1] = 1
+            n -= 1
+
+        return n <= 0
