76ae9617f135c35de42091ebc08eb327c3f7b82e
Sagnik Chandra <98699580+Sagnik120@users.noreply.github.com>
2026-07-14 23:03:57 +0530

    Time: 1 ms (52.81%), Space: 19.4 MB (18.61%) - LeetHub

---
+++ 3545-minimum-deletions-for-at-most-k-distinct-characters/3545-minimum-deletions-for-at-most-k-distinct-characters.py
@@ -0,0 +1,6 @@
+class Solution:
+    def minDeletion(self, s: str, k: int) -> int:
+        ctr = defaultdict(int)
+        for ch in s: ctr[ch]+= 1
+
+        return sum(nsmallest(len(ctr) - k, ctr.values()))
