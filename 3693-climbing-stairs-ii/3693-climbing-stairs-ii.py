0be49e873f30f5f0ec0d6396f094dfcaf516a043
Sagnik Chandra <98699580+Sagnik120@users.noreply.github.com>
2026-07-16 19:40:49 +0530

    Time: 685 ms (38.33%), Space: 33.4 MB (53.46%) - LeetHub

---
+++ 3693-climbing-stairs-ii/3693-climbing-stairs-ii.py
@@ -0,0 +1,13 @@
+class Solution:
+    def climbStairs(self, n: int, costs: List[int]) -> int:
+        INF = float("inf")
+
+        dp = [INF] * (n + 1)
+        dp[0] = 0
+
+        for i in range(1, n + 1):
+            for j in range(max(0, i - 3), i):
+                dp[i] = min(dp[i],
+                            dp[j] + costs[i - 1] + (i - j) ** 2)
+
+        return dp[n]
